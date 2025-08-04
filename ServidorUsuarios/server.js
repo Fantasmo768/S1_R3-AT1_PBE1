eb8db200e3f8b4c56bed93872a76a88d8a15a9ab
unknown <sopesquisa133@gmail.com>
2025-08-04 12:50:21 -0300

    Correção de nome

--- ServidorUsuarios/server.js
+++ ServidorUsuarios/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-const serverUsuarios = express();
+const server = express();
 
 const PORT = 8082;
 
-serverUsuarios.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     console.log(`Esse é o servidor de usuários, rodando na porta ${PORT}`)
 })
